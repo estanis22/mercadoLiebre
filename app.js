aba21cd4b1b693812e90971fad4f5bb9c0366f39
estanis22 <estanislaoperezpesado@gmail.com>
2023-02-27 22:23:03 -0300

    borrando codigo comentado

--- app.js
+++ app.js
@@ -22,26 +22,3 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"))
 });
 
-
-
-/* app.get("/babbage", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/babbage.html"))
-});
-app.get("/berners-lee", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/berners-lee.html"))
-});
-app.get("/clarke", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/clarke.html"))
-});
-app.get("/hamilton", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/hamilton.html"))
-});
-app.get("/hopper", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/hopper.html"))
-});
-app.get("/lovelace", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/lovelace.html"))
-});
-app.get("/turing", (req, res) => {
-    res.sendFile(path.join(__dirname, "/views/turing.html"))
-});*/
